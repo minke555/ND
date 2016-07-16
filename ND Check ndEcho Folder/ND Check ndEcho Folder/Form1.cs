1864cdbf9486d4ceb7afe32bf9127d7e6df1dea8
Justin Bed <justindbedingfield@gmail.com>
2016-07-16 19:52:12 +1000

    test

--- ND Check ndEcho Folder/ND Check ndEcho Folder/Form1.cs
+++ ND Check ndEcho Folder/ND Check ndEcho Folder/Form1.cs
@@ -13,6 +13,18 @@ namespace ND_Check_ndEcho_Folder
 {
     public partial class Form1 : Form
     {
+
+        public String authorizationCode = "";
+        public String baseLoginUrl = "https://au.netdocuments.com/neWeb2/OAuth.aspx";
+        public String baseApiUrl = "https://api.au.netdocuments.com";
+        public String clientID = "AP-HE7KWGHN"; // insert your own client ID here
+        public String clientSecret = "fM5kFT3OoQ4cefrbnx6Y9hZ0Ga81BZ63MgpqpTVdB9lC8U9M"; // insert your own client secret here
+        public String scope = "full";
+        public String responseType = "code";
+        public String redirectURI = "https://localhost/"; // insert your own redirectURI here.  Note that this sample will not work with some redirect URIs as it is written.
+        public String accessToken = "";
+        public String refreshToken = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -25,16 +37,7 @@ namespace ND_Check_ndEcho_Folder
             String ndEchoFolder = (String)ndKey.GetValue("EchoingFolderPath");
             txtndEchoPath.Text = ndEchoFolder;
             // Now lets attempt to connect to ND
-         String authorizationCode = "";
-         String baseLoginUrl = "https://au.netdocuments.com/neWeb2/OAuth.aspx"; 
-         String baseApiUrl = "https://api.au.netdocuments.com"; 
-         String clientID = "AP-HE7KWGHN"; // insert your own client ID here
-         String clientSecret = "fM5kFT3OoQ4cefrbnx6Y9hZ0Ga81BZ63MgpqpTVdB9lC8U9M"; // insert your own client secret here
-         String scope = "full";
-         String responseType = "code";
-         String redirectURI = "https://localhost/"; // insert your own redirectURI here.  Note that this sample will not work with some redirect URIs as it is written.
-         String accessToken = "";
-         String refreshToken = "";
+        
 
     }
 
